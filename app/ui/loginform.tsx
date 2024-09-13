c6b10a0280b281292c4f57ada2f9192398c2c9f3
Wilson Reyhan <jwreyhan@gmail.com>
2024-09-13 12:22:51 -0700

    "Minor CTA change"

--- app/ui/loginform.tsx
+++ app/ui/loginform.tsx
@@ -35,7 +35,7 @@ export default function LoginForm() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h3 className="font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign up for news on drops near you!
+              Sign up for news on drop spots near you!
             </h3>
             <form action={formAction} className="space-y-4 md:space-y-6">
               <div>
